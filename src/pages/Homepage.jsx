9ca809f7ac5ea1039498cf8d7cbfe3279586496f
Shoaib Yousaf <shoaiby31@gmail.com>
2025-07-04 06:15:19 +0500

    work progress of 4-july-2025

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,24 +1,11 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import Header from '../components/header'
 import Features from '../components/features'
 import Whoisit from '../components/whoisit'
 import Testimonials from '../components/testimonials'
 import Contact from '../components/contact'
-import { getAuth } from "firebase/auth";
-
-
-
-
 
 function Homepage() {
-  useEffect(() => {
-  const auth = getAuth();
-  if (auth.currentUser) {
-    auth.currentUser.getIdToken().then(token => {
-      console.log("ID Token:", token);
-    });
-  }
-}, []);
   return (
     <div>
       <Header/>
