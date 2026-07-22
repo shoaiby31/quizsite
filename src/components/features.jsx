81a491f820c0d1629dc3be59c7528e7dee303ec4
Shoaib Yousaf <shoaiby31@gmail.com>
2026-07-22 11:10:15 +0500

    Designing new

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -85,22 +85,22 @@ import { motion } from "framer-motion";
 
 const features = [
   {
-    icon: <QuestionAnswerIcon />,
+    icon: '🎯',
     title: 'Custom Quiz Builder',
     description: 'Create unlimited quizzes with multiple question types (MCQs, True/False, Short Answer, etc.)'
   },
   {
-    icon: <HistoryEduIcon />,
+    icon: '👩‍🏫',
     title: 'For Teachers & Institutes',
     description: 'Easily assign tests to students, track scores, and manage performance data.'
   },
   {
-    icon: <SchoolIcon />,
+    icon: '🧠',
     title: 'For Students & Individuals',
     description: 'Practice for exams or test your knowledge with personalized quizzes.'
   },
   {
-    icon: <AccessTimeFilledIcon />,
+    icon: '🔐',
     title: 'Real-Time Results',
     description: 'Instant feedback with detailed results, rankings, and performance analytics.'
   },
@@ -113,19 +113,19 @@ const Features = () => {
 
     <Box
       sx={{
-        py:2,
-        px:{xs:2,md:6},
-        
+        py: 2,
+        px: { xs: 2, md: 6 },
+
       }}
     >
 
 
-      <motion.div initial={{opacity:0,y:40}} whileInView={{opacity:1,y:0}} viewport={{once:true}} transition={{duration:.7}}>
+      <motion.div initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: .7 }}>
         <Typography variant="h5" fontWeight={800}>
           Why Choose {" "}
           <Box component="span"
-            sx={{ background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", WebkitBackgroundClip:"text", WebkitTextFillColor:"transparent", }}>
-            Smart-Educator</Box>
+            sx={{ background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent", }}>
+            SmartEducator</Box>
           {" "}App</Typography>
 
 
@@ -148,27 +148,31 @@ const Features = () => {
       >
 
         {
-          features.map((item,index)=>(
+          features.map((item, index) => (
 
-            <Grid size={{xs:12, sm:6, md:3}} key={item.title}>
+            <Grid size={{ xs: 12, sm: 6, md: 3 }} key={item.title}>
 
-              <motion.div initial={{ opacity:0, y:50 }} whileInView={{ opacity:1, y:0 }} transition={{ delay:index*.15 }} viewport={{ once:true }} whileHover={{ y:-8 }}>
+              <motion.div initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: index * .15 }} viewport={{ once: true }} whileHover={{ y: -8 }}>
 
                 <Card
-                  sx={{ height:"100%", borderRadius:4, backdropFilter:"blur(10px)", boxShadow: "0 20px 40px rgba(0,0,0,.08)" }}>
+                  sx={{ height: "100%", borderRadius: 4, backdropFilter: "blur(10px)", boxShadow: "0 20px 40px rgba(0,0,0,.08)" }}>
 
                   <CardContent>
-                    <Box sx={{ width:60, height:60, borderRadius:"50%", display:"flex", alignItems:"center", justifyContent:"center", background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", color:"white", mb:2 }}>
-
+                    {/* <Box sx={{ width:60, height:60, borderRadius:"50%", display:"flex", alignItems:"center", justifyContent:"center", background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", color:"white", mb:2 }}>
                       {React.cloneElement(
                         item.icon,
                         { fontSize:"large" }
                       )}
+                    </Box> */}
 
-                    </Box>
+                     <Typography variant="h3" component="div" sx={{textAlign:'center'}} gutterBottom>{item.icon}</Typography>
+                    
+                   
+
+
                     <Typography variant="h6" fontWeight={700}>{item.title}</Typography>
                     <Typography
-                      sx={{ background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", WebkitBackgroundClip:"text", WebkitTextFillColor:"transparent", fontWeight:600 }} >
+                      sx={{ background: "linear-gradient(to top left,hsl(315,93.8%,44.3%),rgb(104,70,253))", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent", fontWeight: 600 }} >
                       {item.role}</Typography>
 
 
